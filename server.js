f4d89e6d4dd1a7863d1aa7b473183d6085e3b47a
Ryan Potter <rjp24@uw.edu>
2016-12-01 16:49:23 -0800

    Add page adding to datbase now

--- server.js
+++ server.js
@@ -3,19 +3,32 @@
 
 // Variables for building the enviroment
 var express = require('express')
-  , logger = require('morgan')
-  , app = express()
-  , template = require('jade').compileFile(__dirname + '/source/templates/homepage.jade')
-  , template2 = require('jade').compileFile(__dirname + '/source/templates/main_page.jade')
-  , template3 = require('jade').compileFile(__dirname + '/source/templates/add_page.jade')
-  , template4 = require('jade').compileFile(__dirname + '/source/templates/edit_page.jade')
-  , template5 = require('jade').compileFile(__dirname + '/source/templates/delete_page.jade')
-  , template6 = require('jade').compileFile(__dirname + '/source/templates/student_report_page.jade')
-  , template7 = require('jade').compileFile(__dirname + '/source/templates/data_report_page.jade')
-  , template8 = require('jade').compileFile(__dirname + '/source/templates/lookup_student_page.jade')
-  , template9 = require('jade').compileFile(__dirname + '/source/templates/loaded_student_page.jade')
-  , template10 = require('jade').compileFile(__dirname + '/source/templates/generated_student_page.jade')
-  , template11 = require('jade').compileFile(__dirname + '/source/templates/students_fulfill_page.jade')
+, logger = require('morgan')
+, app = express()
+, template = require('jade').compileFile(__dirname + '/source/templates/homepage.jade')
+, template2 = require('jade').compileFile(__dirname + '/source/templates/main_page.jade')
+, template3 = require('jade').compileFile(__dirname + '/source/templates/add_page.jade')
+, template4 = require('jade').compileFile(__dirname + '/source/templates/edit_page.jade')
+, template5 = require('jade').compileFile(__dirname + '/source/templates/delete_page.jade')
+, template6 = require('jade').compileFile(__dirname + '/source/templates/student_report_page.jade')
+, template7 = require('jade').compileFile(__dirname + '/source/templates/data_report_page.jade')
+, template8 = require('jade').compileFile(__dirname + '/source/templates/lookup_student_page.jade')
+, template9 = require('jade').compileFile(__dirname + '/source/templates/loaded_student_page.jade')
+, template10 = require('jade').compileFile(__dirname + '/source/templates/generated_student_page.jade')
+, template11 = require('jade').compileFile(__dirname + '/source/templates/students_fulfill_page.jade')
+
+var mysql      = require('mysql');
+var connection = mysql.createConnection({
+	host     : 'cssgate.insttech.washington.edu',
+	user     : '_360team11',
+	password : 'HifOot',
+	database : '_360team11'
+});
+
+connection.connect(function(err) {
+
+});
+
 
 
 app.use(logger('dev'))
@@ -23,118 +36,131 @@ app.use(express.static(__dirname + '/static'))
 
 // The login page
 app.get('/', function (req, res, next) {
-  try {
-    var html = template({ title: 'Home' })
-    res.send(html)
-  } catch (e) {
-    next(e)
-  }
+	try {
+		var html = template({ title: 'Home' })
+		res.send(html)
+	} catch (e) {
+		next(e)
+	}
 })
 
 
 // Page after teh login page
 app.get('/newpage', function (req, res, next) {
-  try {
-    var html = template2({ title: 'Home' })
-    res.send(html)
-  } catch (e) {
-    next(e)
-  }
+	try {
+		var html = template2({ title: 'Home' })
+		res.send(html)
+	} catch (e) {
+		next(e)
+	}
 })
 
 app.get('/add_student', function (req, res, next) {
-  try {
-    var html = template3({ title: 'Home' })
-    res.send(html)
-  } catch (e) {
-    next(e)
-  }
+	try {
+		var html = template3({ title: 'Home' })
+		res.send(html)
+	} catch (e) {
+		next(e)
+	}
 })
 
 app.get('/edit_student', function (req, res, next) {
-  try {
-    var html = template4({ title: 'Home' })
-    res.send(html)
-  } catch (e) {
-    next(e)
-  }
+	try {
+		var html = template4({ title: 'Home' })
+		res.send(html)
+	} catch (e) {
+		next(e)
+	}
 })
 
 app.get('/delete_student', function (req, res, next) {
-  try {
-    var html = template5({ title: 'Home' })
-    res.send(html)
-  } catch (e) {
-    next(e)
-  }
+	try {
+		var html = template5({ title: 'Home' })
+		res.send(html)
+	} catch (e) {
+		next(e)
+	}
 })
 
 app.get('/student_report', function (req, res, next) {
-  try {
-    var html = template6({ title: 'Home' })
-    res.send(html)
-  } catch (e) {
-    next(e)
-  }
+	try {
+		var html = template6({ title: 'Home' })
+		res.send(html)
+	} catch (e) {
+		next(e)
+	}
 })
 
 app.get('/data_report', function (req, res, next) {
-  try {
-    var html = template7({ title: 'Home' })
-    res.send(html)
-  } catch (e) {
-    next(e)
-  }
+	try {
+		var html = template7({ title: 'Home' })
+		res.send(html)
+	} catch (e) {
+		next(e)
+	}
 })
 
 app.get('/lookup_student_report', function (req, res, next) {
+	try {
+		var html = template8({ title: 'Home' })
+		res.send(html)
+	} catch (e) {
+		next(e)
+	}
+})
+
+
+app.get('/submit_add', function (req, res) {
+  // Prepare output in JSON format
+
+  console.log(req.query.f_name + " " + req.query.l_name)
+
+
+  var post  = {id: 3, fName: req.query.f_name}
+  var query = connection.query('INSERT INTO students SET ?', post, function(err, result) {
+
+  })
+  console.log(query.sql)
+
+
   try {
-    var html = template8({ title: 'Home' })
-    res.send(html)
+  	var html = template3({ title: 'Home' })
+  	res.send(html)
   } catch (e) {
-    next(e)
+  	next(e)
   }
 })
 
-<<<<<<< HEAD
-app.get('/submit_add', function (req, res) {
-   // Prepare output in JSON format
-
-   console.log(req.query.f_name + " " + req.query.l_name)
-
-
-   try {
-    var html = template3({ title: 'Home' })
-=======
 app.get('/loaded_student_report', function (req, res, next) {
-  try {
-    var html = template9({ title: 'Home' })
-    res.send(html)
-  } catch (e) {
-    next(e)
-  }
+	try {
+		var html = template9({ title: 'Home' })
+		res.send(html)
+	} catch (e) {
+		next(e)
+	}
 })
 
 app.get('/generated_student_report', function (req, res, next) {
-  try {
-    var html = template10({ title: 'Home' })
-    res.send(html)
-  } catch (e) {
-    next(e)
-  }
+	try {
+		var html = template10({ title: 'Home' })
+		res.send(html)
+	} catch (e) {
+		next(e)
+	}
 })
 
 app.get('/students_fulfill_report', function (req, res, next) {
-  try {
-    var html = template11({ title: 'Home' })
->>>>>>> c7828629394369c9c4dbc3a7fe3c81e7af72419a
-    res.send(html)
-  } catch (e) {
-    next(e)
-  }
+	try {
+		var html = template11({ title: 'Home' })
+
+		res.send(html)
+	} catch (e) {
+		next(e)
+	}
 })
 
 app.listen(process.env.PORT || 3000, function () {
-  console.log('Listening on http://localhost:' + (process.env.PORT || 3000))
+	console.log('Listening on http://localhost:' + (process.env.PORT || 3000))
 })
+
 
