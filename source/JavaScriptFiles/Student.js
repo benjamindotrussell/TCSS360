b196208fb6860c58cb9742d673bb5cd92c89d7a7
benjamindotrussell <hyperorange78@msn.com>
2016-12-03 22:13:49 -0800

    fixed the syntax of the methods
    The methods in Student and StudentCollection finally work in my local project through the localhost site using the add student page. 
    Thanks to Jason for helping me figure out how to export the methods.

--- source/JavaScriptFiles/Student.js
+++ source/JavaScriptFiles/Student.js
@@ -4,60 +4,48 @@
 	Author: Ben Russell
 	Date: 	11/27/2016
 **/
-var Degree 			= require("./Degree");
-var TransferSchool 	= require("./TransferSchool");
+var degree = require('./Degree.js');
+var TransferSchool 	= require("./TransferSchool.js");
+var studentCollection = require('./StudentCollection.js');
 var degreeList = [];
 var transferSchoolList = [];
 var jobList = [];
 /**	Student with fields.**/
-function Student(fName, lName, studentID, graduationTerm, gradYear, externalEmail				, uwEmail, gpa) {
-	this.fName = fName;
-	this.lName = lName;
-	this.studentID = studentID;
-	this.graduationTerm = graduationTerm;
-	this.gradYear = gradYear;
-	this.externalEmail = externalEmail;
-	this.uwEmail = uwEmail;
-	this.gpa = gpa;
-	
-	this.addStudent = addStudent;
-	this.addNewStudent = addNewStudent;
-	this.updateStudent = updateStudent;
-	this.updateStudentJob = updateStudentJob;
-	return this;
-}
+module.exports = {
 
-/** Add a student to the system with all information. **/
-addStudent = function(fName, lName, studentID, degree, degreeLevel, graduationTerm, 					gradYear, externalEmail, uwEmail, gpa) {
-	var student = new Student(fName, lName, studentID, graduationTerm, gradYear, 						externalEmail, uwEmail, gpa);
-	addDegree(degree, degreeLevel, student);
-	return StudentCollection.addStudent(fName, lName, studentID, graduationTerm, gradYear, 						externalEmail, uwEmail, gpa);
-}
+	/** Add a student to the system with all information. **/
+	addStudent: function(fName, lName, studentID, degree, degreeLevel, graduationTerm, 					gradYear, externalEmail, uwEmail, gpa) {
+		//var student = new Student(fName, lName, studentID, graduationTerm, gradYear, 						externalEmail, uwEmail, gpa);
+		addDegree(degree, degreeLevel, student);
+		return StudentCollection.addStudent(fName, lName, studentID, graduationTerm, gradYear, 						externalEmail, uwEmail, gpa);
+	},
 
-/** Add a student to the system with just name and id. **/
-addNewStudent = function(fName, lName, studentID) {
-	var student = new Student(fName, lName, studentID);
-	return StudentCollection.addStudent(fName, lName, studentID);
-}
-/** 
-* Add a degree to the students information.
-* param1: degreeName: name of the degree, 
-* param2: degreeLvl: level of the degreee Bachelors, Masters....
-* param3: student The student to add the degree to.
-* return: boolean Success or failure.
-*/
-addDegree = function(degreeName, degreeLvl, student) {
-	degreeList.push(new Degree(degreeName, degreeLvl));
-	return Degree.addDegree(degreeName, degreeLvl, student.studentID);	
-}
-/** Update a Students basic information **/
-updateStudent = function(fName, lName, studentID, degree, degreeLevel, graduationTerm, 					gradYear, externalEmail, uwEmail, gpa) {	
-	return StudentCollection.updateStudent(fName, lName, studentID, degree, degreeLevel, 				graduationTerm,	gradYear, externalEmail, uwEmail, gpa);
-}
+	/** Add a student to the system with just name and id. **/
+	addNewStudent: function(fName, lName, studentID) {
+		//var student = new Student(fName, lName, studentID);
+		return studentCollection.addStudent(fName, lName, studentID);
+	},
+	/** 
+	* Add a degree to the students information.
+	* param1: degreeName: name of the degree, 
+	* param2: degreeLvl: level of the degreee Bachelors, Masters....
+	* param3: student The student to add the degree to.
+	* return: boolean Success or failure.
+	*/
+	addDegree: function(degreeName, degreeLvl, student) {
+		degreeList.push(new Degree(degreeName, degreeLvl));
+		return Degree.addDegree(degreeName, degreeLvl, student.studentID);	
+	},
+	/** Update a Students basic information **/
+	updateStudent: function(fName, lName, studentID, degree, degreeLevel, graduationTerm, 					gradYear, externalEmail, uwEmail, gpa) {	
+		return StudentCollection.updateStudent(fName, lName, studentID, degree, degreeLevel, 				graduationTerm,	gradYear, externalEmail, uwEmail, gpa);
+	},
 
-/**  Update a Students employment information **/
-updateStudentJob = function(employerName, employeeSalary, startDate, jobTitle) {
-	
-	return JobCollection.addJob(employerName, employeeSalary, startDate, jobTitle);
+	/**  Update a Students employment information **/
+	updateStudentJob: function(employerName, employeeSalary, startDate, jobTitle) {
+		
+		return JobCollection.addJob(employerName, employeeSalary, startDate, jobTitle);
+	}
+
+//module.exports = Student;
 }
-module.exports = Student;
--- source/JavaScriptFiles/StudentCollection.js
+++ source/JavaScriptFiles/StudentCollection.js
@@ -18,160 +18,166 @@ var connection 	= mysql.createConnection({
 connection.connect(function(err) {  
   if (err) { console.log(err);};
 });
-
+module.exports = {
 /**
-* StudentColletion class: A class that intefaces with the DB
-* Param:  none
-* return: none
-**/
-function StudentCollection() {
-	this.addStudent = addStudent;
-	this.retrieveStudent = retrieveStudent;
-	this.deleteStudent = deleteStudent;
-	this.studentReport = studentReport;
-	this.findID = findID;
-}
-/**
-* Retrieve student information from the DB by studentID.
-* Param:  id the studentID of a student
-* return: data A students data.
-**/
-retrieveStudent = function(id){
-	connection.query('SELECT * FROM students WHERE studentID = "' + id + '"', 
-		function(err, data) {
-			try{ 
-				if (err) {
-					console.log(err);
-					return null;
-				} else {
-					console.log(data);
-					return data;
+	* Retrieve student information from the DB by studentID.
+	* Param:  id the studentID of a student
+	* return: data A students data.
+	**/
+	retrieveStudent: function(id){
+		connection.query('SELECT * FROM students WHERE studentID = "' + id + '"', 
+			function(err, data) {
+				try{ 
+					if (err) {
+						console.log(err);
+						return null;
+					} else {
+						console.log(data);
+						return data;
+					}
+				} catch (err){
+					console.log("query failed");
 				}
-			} catch (err){
-				console.log("query failed");
-			}
-	});
-}
-//retrieveStudent('Russell'); //testLine
-/**
-* Retrieve student information from the DB by last name.
-* Param:  lName the last name of a student
-* return: data A students data.
-**/
-retrieveStudent = function(lName){
-	connection.query('SELECT * FROM students WHERE lName = "' + lName + '"', 
-		function(err, data) {
-			try{ 
-				if (err) {
-					console.log(err);
-					return null;
-				} else {
-					return data;
+		});
+	},
+
+	/**
+	* Retrieve student information from the DB by last name.
+	* Param:  lName the last name of a student
+	* return: data A students data.
+	**/
+	retrieveStudent: function(lName){
+		connection.query('SELECT * FROM students WHERE lName = "' + lName + '"', 
+			function(err, data) {
+				try{ 
+					if (err) {
+						console.log(err);
+						return null;
+					} else {
+						return data;
+					}
+				} catch (err){
+					console.log("query failed");
 				}
-			} catch (err){
-				console.log("query failed");
-			}
-	});	
-	return null;
-}
+		});	
+		return null;
+	},
 
-/**
-* Add a studetn to the DB.
-* Param:  Student A student to add 
-* return: boolean whether the query succeded.
-**/
-addStudent = function(fName, lName, studentID, graduationTerm, gradYear, 							externalEmail, uwEmail, gpa) {
-	var post = {lName: lName,
-				fName: fName,
-				studentID: studentID,
-				graduationTerm: graduationTerm, 
-				graduationYear: graduationYear,
-				externalEmail: externalEmail, 
-				uwEmail: uwEmail, 
-				gpa: gpa
-	};
-	connection.query('INSERT INTO students SET ?', post, function(err, result) {
-		if(err) { 
-			console.log(err);
-			return false;
+	/**
+	* Add a studetn to the DB.
+	* Param:  Student A student to add 
+	* return: boolean whether the query succeded.
+
+	addStudent = function(fName, lName, studentID, graduationTerm, gradYear, 							externalEmail, uwEmail, gpa) {
+		var post = {lName: lName,
+					fName: fName,
+					studentID: studentID,
+					graduationTerm: graduationTerm, 
+					graduationYear: graduationYear,
+					externalEmail: externalEmail, 
+					uwEmail: uwEmail, 
+					gpa: gpa
 		};
-	});
-	return true;	
-}
-
-/**
-* remove a student from the DB.
-* Param: student A student to remove.
-* return: booean whether the query succeded.
-**/
-deleteStudent = function(student) {
-	connection.query('DELETE FROM students WHERE studentID = ?', student.studentID,
-			function(err, result) {
-		if(err) { 
-			console.log(err);
-			return false;
+		connection.query('INSERT INTO students SET ?', post, function(err, result) {
+			if(err) { 
+				console.log(err);
+				return false;
+			};
+		});
+		return true;	
+	}
+	**/
+	/**
+	* Add a studetn to the DB.
+	* Param:  Student A student to add 
+	* return: boolean whether the query succeded.
+	**/
+	addStudent: function(fName, lName, studentID) {
+		var post = {lName: lName,
+					fName: fName,
+					studentID: studentID
 		};
-	});
-	return true;
-}
+		connection.query('INSERT INTO students SET ?', post, function(err, result) {
+			if(err) { 
+				console.log(err);
+				return false;
+			};
+		});
+		return true;	
+	},
+	/**
+	* remove a student from the DB.
+	* Param: student A student to remove.
+	* return: booean whether the query succeded.
+	**/
+	deleteStudent: function(student) {
+		connection.query('DELETE FROM students WHERE studentID = ?', student.studentID,
+				function(err, result) {
+			if(err) { 
+				console.log(err);
+				return false;
+			};
+		});
+		return true;
+	},
 
-/**
-* return a list of all students in the database.
-* Param: none 
-* return: array of students.
-**/
-listStudents = function() {
-	connection.query('SELECT * FROM students', 
-		function(err, data) {
-			try{ 
-				if (err) {
-					console.log(err);
-					return null;
-				} else {
-					return data;
+	/**
+	* return a list of all students in the database.
+	* Param: none 
+	* return: array of students.
+	**/
+	listStudents: function() {
+		connection.query('SELECT * FROM students', 
+			function(err, data) {
+				try{ 
+					if (err) {
+						console.log(err);
+						return null;
+					} else {
+						console.log(data);
+						return data;
+					}
+				} catch (err){
+					console.log("query failed");
 				}
-			} catch (err){
-				console.log("query failed");
-			}
-	});
-	return null;
-}
+		});
+		return null;
+	},
 
-updateStudent = function(fName, lName, studentID, degree, degreeLevel, graduationTerm, 					gradYear, externalEmail, uwEmail, gpa) {
-	var post = {lName: lName,
-				fName: fName,
-				graduationTerm: graduationTerm, 
-				graduationYear: graduationYear,
-				externalEmail: externalEmail, 
-				uwEmail: uwEmail, 
-				gpa: gpa
-	};
-	
-	connection.query('UPDATE students SET ? WHERE studentID = studentID'
-		, post, function(err, result) {
-		if(err) { 
-			console.log(err);
-			return false;
+	updateStudent: function(fName, lName, studentID, degree, degreeLevel, graduationTerm, 					gradYear, externalEmail, uwEmail, gpa) {
+		var post = {lName: lName,
+					fName: fName,
+					graduationTerm: graduationTerm, 
+					graduationYear: graduationYear,
+					externalEmail: externalEmail, 
+					uwEmail: uwEmail, 
+					gpa: gpa
 		};
-	});
-	return true;	
-}
+		
+		connection.query('UPDATE students SET ? WHERE studentID = studentID'
+			, post, function(err, result) {
+			if(err) { 
+				console.log(err);
+				return false;
+			};
+		});
+		return true;	
+	},
 
-/**
-* generate a report on the percentage of students who have a job.
-* Param: none
-* return: double.
-**/
-jobReport = function() {
-	return double;
+	/**
+	* generate a report on the percentage of students who have a job.
+	* Param: none
+	* return: double.
+	**/
+	jobReport: function() {
+		return double;
+	},
+	/**
+	* return percentage of students within a gpa range 2.5 - 2.9, 3.0 - 3.4 ......
+	* Param: lowerBound, upperBound,
+	* return: double.
+	**/
+	gpaReport: function(lowerBound, upperBound) {	
+		return double;
+	}
 }
-/**
-* return percentage of students within a gpa range 2.5 - 2.9, 3.0 - 3.4 ......
-* Param: lowerBound, upperBound,
-* return: double.
-**/
-gpaReport = function(lowerBound, upperBound) {	
-	return double;
-}
-//terminate the DB connection
-connection.end();
